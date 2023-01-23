0d2e1bb2b0c53775c22a78de55e5c69edd27f5f0
mostafa-555 <mostafahaman555@gmail.com>
2023-01-23 16:00:35 +0200

    -

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,26 @@
+config.load_autoconfig(True)
+c.auto_save.session = True
+#content settings
+c.content.autoplay = True 
+c.content.canvas_reading = False 
+c.content.cookies.accept = "no-3rdparty"
+c.content.desktop_capture = False
+c.content.geolocation = False  
+c.content.headers.accept_language = 'en-US,en;q=0.5'
+c.content.images = True 
+c.content.javascript.can_access_clipboard = False
+c.content.javascript.can_open_tabs_automatically = False 
+c.content.local_content_can_access_file_urls = False 
+c.content.local_content_can_access_remote_urls = False 
+c.content.media.audio_capture = False
+c.content.media.audio_video_capture = False 
+c.content.media.video_capture = False 
+c.content.mouse_lock = False 
+c.content.notifications.enabled = False 
+c.content.notifications.show_origin = True
+c.content.proxy = 'socks5://localhost:9050/'
+#aliases 
+
+
+
+
